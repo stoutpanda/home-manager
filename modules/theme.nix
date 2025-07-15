f4f8ac118c40568feebfc1f44a2129738019607a
stoutpanda <stoutpanda@protonmail.com>
2025-07-14 19:54:40 -0500

    forcing ff theme

--- modules/theme.nix
+++ modules/theme.nix
@@ -8,7 +8,20 @@
 {
   # Global enable for all supported packages
   catppuccin.enable = true;
-
   # Set the flavor (latte, frappe, macchiato, mocha)
   catppuccin.flavor = "macchiato";
+
+  catppuccin.firefox.profiles = {
+    # These need to match the profile names in your firefox.nix
+    default = {
+      enable = true;
+      # These will inherit from global settings if not specified
+      # flavor = "macchiato";
+      # accent = "mauve";
+    };
+    dev = {
+      enable = true;
+    };
+  };
+
 }
